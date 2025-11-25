1f4a1202231078e385638a98ea21ca7cb55aa128
Sobih <sobihmohamedsobih@gmail.com>
2025-11-25 22:52:54 +0200

    Context od the identity Created

---
+++ Infrastructure/Makanak.Persistance/Contexts/MakanakDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Makanak.Domain.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makanak.Persistance.Contexts
+{
+    public class MakanakDbContext(DbContextOptions<MakanakDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "Users");
+                entity.Property(e => e.Name).IsRequired().HasColumnType("Varchar(100)");
+                entity.Property(e => e.NationalId).HasColumnType("Char(14)");
+            });
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+    }
+}
