2274847e6be466337136c725f4f8517570000b3d
Sobih <sobihmohamedsobih@gmail.com>
2025-11-25 22:33:31 +0200

    Application-User-Module Creation

---
+++ Core/Makanak.Domain/Identity/EnumsHelper/UserTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makanak.Domain.Identity.EnumsHelper
+{
+    public enum UserTypes
+    {
+        Tenant = 1,
+        Owner = 2,
+        Admin = 3
+    }
+}
---
+++ Core/Makanak.Domain/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using Makanak.Domain.Identity.EnumsHelper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makanak.Domain.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        #region Personal Info
+        public string Name { get; set; } = null!;
+        public string? ProfilePictureUrl { get; set; }
+        public string? Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        #endregion
+
+        #region Security Info
+        // but user can't do any thing without verification by send national id images
+        public string? NationalId { get; set; } = null!;
+        public string? NationalIdImageFrontUrl { get; set; } = null!;
+        public string? NationalIdImageBackUrl { get; set; } = null!;
+
+        #endregion
+        public int StrikeCount { get; set; } = 0;
+        // User type : Tenant, Owner, Admin
+        public UserTypes UserType { get; set; }
+        // User status : Pending, Active, Rejected, Banned
+        public UserStatus UserStatus { get; set; } = UserStatus.Pending;
+        public string? RejectedReason { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
